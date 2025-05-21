d174697d91249387e4bf2823eb6291a6d93bd33e
Lokheshrj <lokheshrj@gmail.com>
2025-05-21 14:40:23 +0530

    Initial Setup

--- src/PortfolioForm.js
+++ src/PortfolioForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-
+//changed
 export default function PortfolioForm() {
     const [formData, setFormData] = useState({
         full_name: '',
